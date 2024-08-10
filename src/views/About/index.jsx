d3080f6cb058bd8780d3bd74a8d1de06bfa98c00
Joen AO <anayajoen@gmail.com>
2024-08-10 13:55:05 -0500

    about me, y otros detalles

--- src/views/About/index.jsx
+++ src/views/About/index.jsx
@@ -6,7 +6,29 @@ const About = () => {
    return(
     <div className={styles.aboutContainer}>
         <Navbar/>
-        <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Qui nobis repellat vel temporibus, similique repudiandae pariatur corrupti harum tempora numquam dignissimos, itaque, quasi sint iste. Minus nulla dolorum doloremque tempora.</p>
+        <div className={styles.containerText}>
+            <p className={styles.title}>Potencia tus proyectos con un Front-end eficiente y creativo.</p>
+            <p className={styles.description}>Eleve tu negocio digital a otro nivel con un Front-end de calidad!
+            ¡Hola! Soy Joen Anaya, desarrollador Front-end especializado en React, HTML, CSS y JavaScript. Me apasiona convertir ideas en realidades digitales y apoyar a emprendedores y diseñadores a crear experiencias web impactantes. ¿Conectamos?
+            </p>
+        
+            <div className={styles.containerEnlaces}>
+                <h2 className={styles.enlacesTitle}>Accede a mis redes</h2>
+                <a className={styles.enlacesLink} href="https://github.com/joen-ao/">
+                    <img src='/src/assets/github.png' className={styles.imgButton} />
+                            Github
+                </a>
+                <a className={styles.enlacesLink}  href="https://linkedin.com/in/joen-anaya">
+                    <img src='/src/assets/linkedin.png' className={styles.imgButton} />
+                            Linkedin
+                </a>
+            </div>
+        
+        </div>
+        
+
+        <img src='/src/assets/fb.jpg' alt='Foto de presentacion' className={styles.imgPresentacion}/>
+        
         <Footer/>
     </div>
    ) 
